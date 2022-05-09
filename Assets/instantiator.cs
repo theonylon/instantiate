04a618f6b927fb67b986c96b8d549835260f3a0b
theonlyone <102159023+theonylon@users.noreply.github.com>
2022-05-09 10:30:24 -0300

    exercise done

---
+++ Assets/instantiator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloneObject ()
+    {
+        Instantiate(objectToClone);
+    }
+}
